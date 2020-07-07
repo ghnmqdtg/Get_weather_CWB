6e1efaee36cb49bf11aad004b4c4937002124d8f
ghnmqdtg <ghnmqdtg@gmail.com>
2020-07-07 23:30:44 +0800

    Fix: Add function name checking process

--- get_current.py
+++ get_current.py
@@ -7,40 +7,44 @@ config.read("config.ini")
 
 
 def Get_Current(functions, location):
-    # URL for fetching the current weather
-    Request_URL = config["URL"]["observation_auto"]\
-        + config["settings"]["Authorization"] + "&format=JSON"\
-        + "&locationName=" + urllib.parse.quote(location)
+    if(functions == "目前天氣"):
+        # URL for fetching the current weather
+        Request_URL = config["URL"]["observation_auto"]\
+            + config["settings"]["Authorization"] + "&format=JSON"\
+            + "&locationName=" + urllib.parse.quote(location)
 
-    # data = requests.get(Request_URL).json()
-    # print(data)
+        # data = requests.get(Request_URL).json()
+        # print(data)
 
-    # filter out which data we need
-    data = requests.get(Request_URL).json()["records"]["location"]
+        # filter out which data we need
+        data = requests.get(Request_URL).json()["records"]["location"]
 
-    # examine if the location is valid
-    if not data:
-        observed_data = "target station not found"
-        # print(target_station)
-        return False
+        # examine if the location is valid
+        if not data:
+            observed_data = "target station not found"
+            # print(target_station)
+            return False
 
-    # load message
-    # ref: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0001-001.pdf
-    city = data[0]["parameter"][0]["parameterValue"]
-    town = data[0]["parameter"][2]["parameterValue"]
-    observed_data = data[0]["weatherElement"]
+        # load message
+        # ref: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0001-001.pdf
+        city = data[0]["parameter"][0]["parameterValue"]
+        town = data[0]["parameter"][2]["parameterValue"]
+        observed_data = data[0]["weatherElement"]
 
-    temp = observed_data[3]["elementValue"]
-    humidity = str(float(observed_data[4]["elementValue"]) * 100)
-    # wind_dir = observed_data[1]["elementValue"]
-    # wind_speed = observed_data[0]["elementValue"]
+        temp = observed_data[3]["elementValue"]
+        humidity = str(float(observed_data[4]["elementValue"]) * 100)
+        # wind_dir = observed_data[1]["elementValue"]
+        # wind_speed = observed_data[0]["elementValue"]
 
-    msg = city + town + " " + functions + "\n"
-    msg += "氣溫：" + temp + "℃\n"
-    msg += "濕度：" + humidity + "% RH\n"
-    # msg += "風向：" + wind_dir + "°\n"
-    # msg += "風速：" + wind_speed + " m/s\n"
-    print(msg)
+        msg = city + town + " " + functions + "\n"
+        msg += "氣溫：" + temp + "℃\n"
+        msg += "濕度：" + humidity + "% RH\n"
+        # msg += "風向：" + wind_dir + "°\n"
+        # msg += "風速：" + wind_speed + " m/s\n"
+        print(msg)
+
+    else:
+        print("功能名稱錯誤")
 
 
 Get_Current("目前天氣", "永和")
--- get_predic_36h.py
+++ get_predic_36h.py
@@ -7,63 +7,67 @@ config.read("config.ini")
 
 
 def Get_36hours(functions, location):
-    # URL for fetching the current weather
-    Request_URL = config["URL"]["prediction_36hours"]\
-        + config["settings"]["Authorization"] + "&format=JSON"\
-        + "&locationName=" + urllib.parse.quote(location)
+    if(functions == "三天預報"):
+        # URL for fetching the current weather
+        Request_URL = config["URL"]["prediction_36hours"]\
+            + config["settings"]["Authorization"] + "&format=JSON"\
+            + "&locationName=" + urllib.parse.quote(location)
 
-    # filter out which data we need
-    raw = requests.get(Request_URL).json()["records"]
-    function = raw["datasetDescription"]
-    data = raw["location"][0]
+        # filter out which data we need
+        raw = requests.get(Request_URL).json()["records"]
+        function = raw["datasetDescription"]
+        data = raw["location"][0]
 
-    # examine if the location is valid
-    if not data:
-        predict_data = "target city not found"
-        # print(target_station)
-        return False
+        # examine if the location is valid
+        if not data:
+            predict_data = "target city not found"
+            # print(target_station)
+            return False
 
-    # load message
-    # ref: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0001-001.pdf
-    predict_data = data["weatherElement"]
-    city = data["locationName"]
-    list_results = []
+        # load message
+        # ref: https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0001-001.pdf
+        predict_data = data["weatherElement"]
+        city = data["locationName"]
+        list_results = []
 
-    # fetch three intervals of time
-    for fetch_time in predict_data[0]["time"]:
-        # fetch date
-        date_start = fetch_time["startTime"][5:10].replace("-", "/")
-        date_end = fetch_time["endTime"][8:10]
-        date = date_start + "~" + date_end
+        # fetch three intervals of time
+        for fetch_time in predict_data[0]["time"]:
+            # fetch date
+            date_start = fetch_time["startTime"][5:10].replace("-", "/")
+            date_end = fetch_time["endTime"][8:10]
+            date = date_start + "~" + date_end
 
-        # fetch duration
-        duration_start = fetch_time["startTime"][11:16]
-        duration_end = fetch_time["endTime"][11:16]
-        duration = duration_start + "~" + duration_end
+            # fetch duration
+            duration_start = fetch_time["startTime"][11:16]
+            duration_end = fetch_time["endTime"][11:16]
+            duration = duration_start + "~" + duration_end
 
-        # save results into list
-        list_results.append(date + " " + duration)
+            # save results into list
+            list_results.append(date + " " + duration)
 
-    i = 0
-    j = 0
+        i = 0
+        j = 0
 
-    for x in predict_data:
-        for y in x["time"]:
-            situation = y["parameter"]["parameterName"]
-            if(i % 3 == 1):
-                situation = situation + "%"
-            elif(i % 3 == 2 or i % 3 == 4):
-                situation = situation + "℃"
+        for x in predict_data:
+            for y in x["time"]:
+                situation = y["parameter"]["parameterName"]
+                if(i % 3 == 1):
+                    situation = situation + "%"
+                elif(i % 3 == 2 or i % 3 == 4):
+                    situation = situation + "℃"
 
-            # update the list with Wx, Pop, MinT, CI and MaxT
-            list_results[j % 3] += " " + situation
-            j = j + 1
+                # update the list with Wx, Pop, MinT, CI and MaxT
+                list_results[j % 3] += " " + situation
+                j = j + 1
 
-        i = i + 1
+            i = i + 1
 
-    print(function + "：" + city)
-    for content in list_results:
-        print(content)
+        print(function + "：" + city)
+        for content in list_results:
+            print(content)
+
+    else:
+        print("功能名稱錯誤")
 
 
 Get_36hours("目前天氣", "臺北市")
