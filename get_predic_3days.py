36a88efca0e3853d3a3790e4a0ac5da74fe4de73
ghnmqdtg <ghnmqdtg@gmail.com>
2020-07-13 23:41:06 +0800

    Feat: Update 3 days prediction output as JSON format
    The output is easier for the user to fetch data by using for loop.

--- get_predic_3days.py
+++ get_predic_3days.py
@@ -6,11 +6,11 @@ import json
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-
+'''
 def List_Dict_Converter(lst):
     res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
     return res_dct
-
+'''
 
 def Get_Predic_3Days(functions, location):
     if(functions == "三天預報"):
@@ -25,23 +25,40 @@ def Get_Predic_3Days(functions, location):
         if(response.status_code == requests.codes.ok):
             # filter out which data we need
             raw = response.json()["records"]["locations"][0]
-            function_type = "weather"
             data = raw["location"][0]["weatherElement"]
+
+            function_type = "Weather"
+            description = functions
             list_results = []
 
             # short form: Wx, PoP, AT
             index = [1, 7, 2]
             # used when coverting list into dict
-            keyname = ["Type", "Description", "Location", "Date", "Time", "Wx", "PoP6h", "AT"]
+            dict_output = {
+                "Type": function_type,
+                "Description": description,
+                "Location": location,
+                "Prediction": {
+                    "Date": [],
+                    "Time": [],
+                    "Wx": [],
+                    "PoP6h": [],
+                    "AT": []
+                }
+            }
 
-            # Long form: Wx, PoP, AT, T, RH, CI
             '''
+            # Long form: Wx, PoP, AT, T, RH, CI
             index = [1, 7, 2, 3, 4, 5]
-            keyname = ["Type", "Description", "Location", "Date", "Time", "Wx", "PoP6h", "AT", "T", "RH", "CI"]
+            dict_output = {
+                "Type": function_type, "Description": description,
+                "Location": location, "Prediction": {"Date": [],
+                "Time": [], "Wx": [], "PoP6h": [], "AT": [],
+                "T": [], "RH": [], "CI": []}}
             '''
 
             i = 0
-
+            # Categorize data into strings
             for idx, key in enumerate(index):
                 if(idx == 0):
                     for fetch_time in data[key]["time"]:
@@ -56,7 +73,6 @@ def Get_Predic_3Days(functions, location):
                         duration = duration_start + "~" + duration_end
 
                         # fetch value
-                        # value = "|{0:<7}|".format(fetch_time["elementValue"][0]["value"]).replace(" ", "　")
                         value = fetch_time["elementValue"][0]["value"]
                         output = date + " " + duration + " " + value
                         # save results into list
@@ -64,9 +80,7 @@ def Get_Predic_3Days(functions, location):
 
                 elif(idx == 1):
                     for fetch_time in data[key]["time"]:
-                        # value = "{0:>2}".format(fetch_time["elementValue"][0]["value"])
                         value = " " + fetch_time["elementValue"][0]["value"]
-                        # output = data[key]["description"][3:] + "：" + value + "%"
                         output = value + "%"
                         list_results[i] = list_results[i] + output
                         list_results[i + 1] = list_results[i + 1] + output
@@ -81,26 +95,20 @@ def Get_Predic_3Days(functions, location):
                         list_results[i] = list_results[i] + output
                         i = i + 1
 
-            # Steps: string > list > dict with keys > final list for output
+            # Prepar for the output dict
+            # Steps: string > split into list > save into the list in dict
+            load = dict_output["Prediction"]
+            i = 0
             for order, content in enumerate(list_results):
-                # split the string into list
-                list_results[order] = content.split(" ")
-
-                # insert keys into the list which is to be converted into dict
-                list_results[order].insert(0, function_type)
-                list_results[order].insert(1, functions)
-                list_results[order].insert(2, location)
-
-                for num in range(0, len(keyname)):
-                    list_results[order].insert(num * 2, keyname[num])
-                # print(list_results[order])  # for testing
-
-            # Convert a specified range of data of list into dict
-            # Finally append dict into final list
-            final_output = []
-            for order in range(0, 3):
-                final_output.append(List_Dict_Converter(list_results[order]))
-            return final_output
+                # determine how many data to be loaded
+                if(i < 4):
+                    # split the string into list
+                    list_results[order] = content.split(" ")
+                    # save elements of the list into the output list in dict
+                    for j, keys in enumerate(load):
+                        load[keys].append(list_results[order][j])
+                i = i + 1
+            return dict_output
 
         else:
             print("Request fialed, status_code:", response.status_code)
@@ -111,40 +119,50 @@ def Get_Predic_3Days(functions, location):
 output = Get_Predic_3Days("三天預報", "台北市")
 
 if(output):
-    for i in range(0, 3):
-        print(json.dumps(output[i], indent=4, ensure_ascii=False))
+    # JSON format
+    print(json.dumps(output, indent=4, ensure_ascii=False))
+    # dict format
+    # print(output["Prediction"], "\n")
+    # print(output["Prediction"]["Date"], "\n")
+    # print(output["Prediction"]["Time"][2], output["Prediction"]["Wx"][2])
 
-
-# Results
+ # Results_JSON_format
 '''
 {
-    "Type": "weather",
+    "Type": "Weather",
     "Description": "三天預報",
     "Location": "台北市",
-    "Date": "07/13~13",
-    "Time": "18:00~21:00",
-    "Wx": "晴",
-    "PoP6h": "0%",
-    "AT": "36"
-}
-{
-    "Type": "weather",
-    "Description": "三天預報",
-    "Location": "台北市",
-    "Date": "07/13~14",
-    "Time": "21:00~00:00",
-    "Wx": "晴",
-    "PoP6h": "0%",
-    "AT": "34"
-}
-{
-    "Type": "weather",
-    "Description": "三天預報",
-    "Location": "台北市",
-    "Date": "07/14~14",
-    "Time": "00:00~03:00",
-    "Wx": "晴",
-    "PoP6h": "0%",
-    "AT": "32"
+    "Prediction": {
+        "Date": [
+            "07/14~14",
+            "07/14~14",
+            "07/14~14",
+            "07/14~14"
+        ],
+        "Time": [
+            "00:00~03:00",
+            "03:00~06:00",
+            "06:00~09:00",
+            "09:00~12:00"
+        ],
+        "Wx": [
+            "晴",
+            "多雲",
+            "晴",
+            "晴"
+        ],
+        "PoP6h": [
+            "0%",
+            "0%",
+            "0%",
+            "0%"
+        ],
+        "AT": [
+            "32",
+            "31",
+            "30",
+            "37"
+        ]
+    }
 }
 '''
